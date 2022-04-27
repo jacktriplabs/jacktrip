3f19e9ad7b8cf6a7ee6090b0adf2cdd3d7f9ea97
Matt Horton <matthew.a.horton@gmail.com>
2022-04-27 13:51:59 -0500

    Fix nogui linux build

--- src/main.cpp
+++ src/main.cpp
@@ -216,8 +216,10 @@ bool isRunFromCmd()
 
 int main(int argc, char* argv[])
 {
+#ifndef NO_GUI
     // turn on the DPI support**
     QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+#endif
 
     QScopedPointer<QCoreApplication> app(createApplication(argc, argv));
     QScopedPointer<JackTrip> jackTrip;
